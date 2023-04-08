5b6d77767896bfc10eb24193afebdba6aafefadb
JI3BA <lyovkin_artyom@mail.ru>
2023-04-08 19:00:27 +0300

    added components files and fix index.tsx files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const Header: FC = () => {
+    return(
+        <div className='header'>
+            <div className='header__container wrapper'>
+             
+            </div>
+        </div>
+    )
+}
+
+export default Header
