1b9eaa8a352dd327a20a6d87814767887b1ee465
JI3BA <lyovkin_artyom@mail.ru>
2023-04-08 20:09:08 +0300

    added component Header with styles and fix fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,32 @@
 import { FC } from 'react'
+import './Header.scss'
 
 const Header: FC = () => {
     return(
         <div className='header'>
             <div className='header__container wrapper'>
-             
+                <div className="header__links">
+                    <div className='link__container'>
+                        <a href="#main" className="link__text">Main</a>
+                        <p className="link__line"></p>
+                    </div>
+                    <div className='link__container'>
+                        <a href="#about" className="link__text">About me</a>
+                        <p className="link__line"></p>
+                    </div>
+                    <div className='link__container'>
+                        <a href="#skills" className="link__text">Skills</a>
+                        <p className="link__line"></p>
+                    </div>
+                    <div className='link__container'>
+                        <a href="#location" className="link__text">Location</a>
+                        <p className="link__line"></p>
+                    </div>
+                    <div className='link__container'>
+                        <a href="#gallery" className="link__text">Gallery</a>
+                        <p className="link__line"></p>
+                    </div>
+                </div>
             </div>
         </div>
     )
