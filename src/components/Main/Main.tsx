b063c68d4c3a295ae198ec279834ae3c96b39411
JI3BA <lyovkin_artyom@mail.ru>
2023-04-09 17:36:18 +0300

    added declare for picture and created About me component with styles and some animation blocks, fix some component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import './Main.scss'
+import About from './About/About'
+
+const Main: FC = () => {
+    return(
+        <>
+            <About />
+        </>
+    )
+}
+
+export default Main
