875d8639dfa9fba86cab7946559ac35e6d07f731
JI3BA <lyovkin_artyom@mail.ru>
2023-04-13 12:23:02 +0300

    added Skill component with style and created skills JSON file for skills data and some fix components

---
+++ src/components/Main/Skills/Skill/Skill.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import { motion } from "framer-motion";
+import './Skill.scss'
+
+type SkillProps = {
+    name: string,
+    procent: number
+}
+
+const Skill: FC<SkillProps> = ({name, procent}) => {
+    return(
+            <motion.div className="skills__skill" initial={{strokeDasharray: 472, strokeDashoffset: 472}} whileInView={{strokeDashoffset:  472 - (472 * (procent / 100))}} viewport={{once: true}} transition={{duration: 2}}>
+                <div className='skill__outer'>
+                    <div className="skill__inner">
+                        <p className="skill_procent">{procent}%</p>
+                    </div>
+                </div>
+                            
+                <svg className="skill__svg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+                    <defs>
+                        <linearGradient id="gradient">
+                        <stop offset="0%" stopColor="#d400ff" />
+                        <stop offset="100%" stopColor="#c23802" />
+                        </linearGradient>
+                    </defs>
+                                
+                    <motion.circle
+                        className='skill__circle'
+                        cx="80"
+                        cy="80"
+                        r="70"
+                        strokeLinecap="round"
+                    />
+                    </svg>
+
+                    <p className="skills__name">{name}</p>
+                </motion.div>
+    )
+}
+
+export default Skill
