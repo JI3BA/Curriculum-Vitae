c612af7426cfea63eab39ae71ca4ae42076d620d
JI3BA <lyovkin_artyom@mail.ru>
2023-07-11 17:10:28 +0300

    added Link from router dom

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,45 @@
 import './Header.scss'
+import {Link} from "react-router-dom";
 
 export const Header = () => {
     return(
         <div className='header'>
             <div className='header__container wrapper'>
                 <div className="header__links">
-                    <div className='link__container'>
-                        <a href="#about" className="link__text">About me</a>
-                        <p className="link__line"></p>
-                    </div>
-                    <div className='link__container'>
-                        <a href="#education" className="link__text">Education</a>
-                        <p className="link__line"></p>
-                    </div>
-                    <div className='link__container'>
-                        <a href="#skills" className="link__text">Skills</a>
-                        <p className="link__line"></p>
-                    </div>
-                    <div className='link__container'>
-                        <a href="#projects" className="link__text">Projects</a>
-                        <p className="link__line"></p>
-                    </div>
-                    <div className='link__container'>
-                        <a href="#gallery" className="link__text">Gallery</a>
-                        <p className="link__line"></p>
-                    </div>
+                    <Link to='about' className="link__text">
+                        <div className='link__container'>
+                            <p className="link__text">About me</p>
+                            <p className="link__line"></p>
+                        </div>
+                    </Link>
+
+                    <Link to='education' className="link__text">
+                        <div className='link__container'>
+                            <p className="link__text">Education</p>
+                            <p className="link__line"></p>
+                        </div>
+                    </Link>
+
+                    <Link to='skills' className="link__text">
+                        <div className='link__container'>
+                            <p className="link__text">Skills</p>
+                            <p className="link__line"></p>
+                        </div>
+                    </Link>
+
+                    <Link to='projects' className="link__text">
+                        <div className='link__container'>
+                            <p className="link__text">Projects</p>
+                            <p className="link__line"></p>
+                        </div>
+                    </Link>
+
+                    <Link to='gallery' className="link__text">
+                        <div className='link__container'>
+                            <p className="link__text">Gallery</p>
+                            <p className="link__line"></p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
