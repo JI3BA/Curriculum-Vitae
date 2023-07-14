0c2a5bcae90e9114f438bedb0b319cdf6ac346fa
JI3BA <lyovkin_artyom@mail.ru>
2023-07-14 18:08:57 +0300

    fixed Main component (changed different Route)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,17 +4,17 @@ import { Education } from '../../pages/Education/Education'
 import { Skills } from '../../pages/Skills/Skills'
 import { Projects } from '../../pages/Projects/Projects'
 import { Gallery } from '../../pages/Gallery/Gallery'
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 
 export const Main = () => {
     return(
         <Routes>
-            <Route path='*' element={<About />} />
-            <Route path='/' element={<About />} />
-            <Route path='/education' element={<Education />} />
-            <Route path='/skills' element={<Skills />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/gallery' element={<Gallery />} />
+            <Route path='*' element={<Navigate to="about" replace />} />
+            <Route path='about' element={<About />} />
+            <Route path='education' element={<Education />} />
+            <Route path='skills' element={<Skills />} />
+            <Route path='projects' element={<Projects />} />
+            <Route path='gallery' element={<Gallery />} />
         </Routes>
     )
 }
