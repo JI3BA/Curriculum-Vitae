c69db445e4915a94fafae341ec603351b8f12306
JI3BA <lyovkin_artyom@mail.ru>
2023-08-25 17:47:20 +0300

    added arrow to speciality

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -24,7 +24,11 @@ export const Education = () => {
                         </div>
 
                         <p className="education__speciality" onClick={() => setShowSpec(prev => !prev)}>Automation technological processes and
-                        production (automation engineer)
+                        production (automation engineer) <span >
+                                <svg className='education__speciality--arrow' xmlns="http://www.w3.org/2000/svg" width='20' height='20' viewBox="0 0 32 32">
+                                    <path fill="#ffffff" d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/>
+                                </svg>
+                            </span>
                         </p>
 
                         <p className={showSpec ? "education__speciality--description active" : "education__speciality--description"}>
