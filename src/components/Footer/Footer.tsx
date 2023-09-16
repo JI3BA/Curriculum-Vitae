ccc19c5538f0bb59ffab2bd58f803e20e9a23215
JI3BA <lyovkin_artyom@mail.ru>
2023-09-16 20:44:33 +0300

    link for my email was added to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div className="footer">
             <div className="footer__container wrapper">
                 <a href="https://www.linkedin.com/in/artyom-lyovkin-170bb9215/" rel="noreferrer" target="_blank" className="footer__link">Artyom Lyovkin</a>
-                <p className="footer__email">Contact: artyomlyovkin@gmail.com</p>
+                <p className="footer__email">Contact: <a className='contact__link' target='_blank' rel="noreferrer"  href='mailto:artyomlyovkin@gmail.com'>artyomlyovkin@gmail.com</a></p>
             </div>
         </div>
     )
